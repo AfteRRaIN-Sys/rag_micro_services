3f9afb91a9b075cb2db52567cd1a2c4952dc675d
phet <phet610@hotmail.com>
2025-04-05 22:19:42 +0700

    vector db WIP

--- vector_db/app.py
+++ vector_db/app.py
@@ -1,10 +1,65 @@
+from typing import Iterable
+
+# processing
 import numpy as np
 import pandas as pd
+import torch
+from transformers import AutoModel, AutoTokenizer
 
-import transformers
+# db
+import chromadb
 
-def main() :
+
+class TextEncoderWrapper:
+
+    def __init__(self, ckpt: str):
+        self.ckpt = ckpt
+        self.tokenizer = AutoTokenizer.from_pretrained(ckpt)
+        self.model = AutoModel.from_pretrained(ckpt)
+
+    def encode_tokens(self, tokens: np.ndarray):
+        tokens = torch.from_numpy(tokens)
+        return self.model(tokens)
+
+    def __call__(self, texts: Iterable[str]) -> np.ndarray:
+
+        model_input = self.tokenizer(
+            texts, padding=True, truncation=True, return_tensors="pt"
+        )
+
+        model_output = self.model(**model_input)
+
+        mask: torch.Tensor = model_input["attention_mask"]
+        # mask = mask.unsqueeze(-1).expand()
+
+
+class CustomVectorDatabase:
+
+    def __init__(self, encoder: TextEncoderWrapper):
+
+        # init text encoder
+        self.encoder = encoder
+
+        # init chromadb client
+        chroma_client = chromadb.HttpClient(host="localhost", port=8000)
+
+    def retrive_relevant_context(query_text: str, k=5):
+        pass
+
+    def dump_data_to_db(text_data: Iterable[str]):
+        pass
+
+
+def main():
     pass
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     print("start app!")
+
+    texts = ["This is text 1!", "This is text 2!"]
+
+    ckpt = "sentence-transformers/all-MiniLM-L6-v2"
+    vector_db = CustomVectorDatabase(encoder=TextEncoderWrapper(ckpt))
+
+    vector_db.dump_data_to_db(texts)
